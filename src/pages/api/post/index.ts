e187bc8a528dec1176eacd6a3067e84322d38d98
muttaqinrizal <rizalm57@gmail.com>
2023-07-16 21:51:58 +0700

    feat : post

---
+++ src/pages/api/post/index.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../../lib/prisma'
+
+// POST /api/post
+// Required fields in body: title, authorEmail
+// Optional fields in body: content
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { title, content, authorEmail } = req.body
+  console.log(req.body);
+  
+  let id = Date.now()
+  const result = await prisma.post.create({
+    data: {
+      id: id,
+      title: title,
+      content: content,
+      author: { connect: { email: authorEmail } },
+    },
+  })
+  return res.status(201).json(result)
+}
