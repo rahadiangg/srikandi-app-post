f38c59076c6d30fcb19085ffc01e3451dac07814
muttaqinrizal <rizalm57@gmail.com>
2023-07-16 22:06:03 +0700

    feat : handle cors

--- src/pages/api/post/[id].ts
+++ src/pages/api/post/[id].ts
@@ -1,10 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../../lib/prisma'
+import NextCors from 'nextjs-cors';
 
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+ });
+
   const postId = req.query.id
 
   switch (req.method) {
--- src/pages/api/post/index.ts
+++ src/pages/api/post/index.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../../lib/prisma'
+import NextCors from 'nextjs-cors';
 
 // POST /api/post
 // Required fields in body: title, authorEmail
@@ -8,9 +9,16 @@ export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+ });
+
   const { title, content, authorEmail } = req.body
-  console.log(req.body);
-  
+    
   let id = Date.now()
   const result = await prisma.post.create({
     data: {
